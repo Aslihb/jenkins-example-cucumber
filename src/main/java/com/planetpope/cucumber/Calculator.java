2beb99af920a4f4aa652a5f27105fc14313e4b16
ramazanozersf <brsheres@gmail.com>
2022-07-05 13:56:32 -0700

    first commit

---
+++ src/main/java/com/planetpope/cucumber/Calculator.java
@@ -0,0 +1,14 @@
+package com.planetpope.cucumber;
+
+public class Calculator {
+  public Calculator() {}
+
+  public int add(int x,int y) {
+    return x+y;
+  }
+
+  public int difference(int x,int y) {
+    return x-y;
+  }
+  
+}
